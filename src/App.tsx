51c44dc46498abe8855d5c69384dfbb226f43eef
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2025-04-14 19:32:36 -0300

    Repositions password fields and adds eye icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Eye } from '@phosphor-icons/react';
+
 function App() {
    return (
       <main className='min-h-screen flex flex-col items-center justify-center bg-zinc-900 md:p-4'>
@@ -162,36 +164,47 @@ function App() {
                   />
                </div>
 
-               <div className='space-y-2'>
-                  <label
-                     htmlFor='password'
-                     className='block text-md font-medium px-1 text-zinc-300'
-                  >
-                     Senha
-                  </label>
-                  <input
-                     type='password'
-                     id='password'
-                     name='password'
-                     required
-                     className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-zinc-100 placeholder-zinc-400 focus:outline-none focus:ring-2 focus:ring-gray-500'
-                  />
-               </div>
-
-               <div className='space-y-2'>
-                  <label
-                     htmlFor='confirmPassword'
-                     className='block text-md font-medium px-1 text-zinc-300'
-                  >
-                     Confirmar senha
-                  </label>
-                  <input
-                     type='password'
-                     id='confirmPassword'
-                     name='confirmPassword'
-                     required
-                     className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-zinc-100 placeholder-zinc-400 focus:outline-none focus:ring-2 focus:ring-gray-500'
-                  />
+               <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                  <div className='space-y-2'>
+                     <label
+                        htmlFor='password'
+                        className='block text-md font-medium px-1 text-zinc-300'
+                     >
+                        Senha
+                     </label>
+                     <div className='relative'>
+                        <input
+                           type='password'
+                           id='password'
+                           name='password'
+                           required
+                           className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-zinc-100 placeholder-zinc-400 focus:outline-none focus:ring-2 focus:ring-gray-500'
+                        />
+                        <span className='absolute right-3 top-2'>
+                           <Eye size={24} />
+                        </span>
+                     </div>
+                  </div>
+                  <div className='space-y-2'>
+                     <label
+                        htmlFor='confirmPassword'
+                        className='block text-md font-medium px-1 text-zinc-300'
+                     >
+                        Confirmar senha
+                     </label>
+                     <div className='relative'>
+                        <input
+                           type='password'
+                           id='confirmPassword'
+                           name='confirmPassword'
+                           required
+                           className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-zinc-100 placeholder-zinc-400 focus:outline-none focus:ring-2 focus:ring-gray-500'
+                        />
+                        <span className='absolute right-3 top-2'>
+                           <Eye size={24} />
+                        </span>
+                     </div>
+                  </div>
                </div>
             </fieldset>
 
