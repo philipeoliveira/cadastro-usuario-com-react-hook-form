03bae73b665883c596ae2d50c9307850ae4e0fdb
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2025-04-15 12:16:22 -0300

    Add shadow to h1 text

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ function App() {
    return (
       <main className='min-h-screen flex flex-col items-center justify-center md:p-4'>
          <div className='flex flex-col items-center justify-center gap-1 w-full max-w-3xl bg-linear-to-t from-teal-600 to-teal-800 md:rounded-t-lg border-b border-teal-500 p-4 pb-5'>
-            <h1 className='text-2xl font-semibold'>Cadastro de Usuário</h1>
+            <h1 className='text-2xl font-semibold text-shadow-sm text-shadow-teal-800'>
+               Cadastro de Usuário
+            </h1>
             <h2 className='text-zinc-300'>Formulário com React Hook Form e Zod</h2>
          </div>
          <form className='w-full max-w-3xl bg-zinc-800 rounded-b-lg shadow-xl p-6 space-y-6'>
