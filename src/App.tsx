b89d1fcafe14da7d0c6c1e99789e8af53570d008
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2025-04-23 11:36:00 -0300

    Adapt the form with hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
-import { Eye, EyeSlash } from '@phosphor-icons/react';
-import { withMask } from 'use-mask-input';
+import { Eye, EyeSlash, SpinnerGap } from '@phosphor-icons/react';
+import { useHookFormMask } from 'use-mask-input';
+import { FieldValues, useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 
 function App() {
    const [isPasswordVisible, setIsPasswordVisible] = useState(false);
    const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState(false);
    const [address, setAddress] = useState({ city: '', state: '' });
+
+   const {
+      register,
+      handleSubmit,
+      formState: { isSubmitting, errors },
+   } = useForm();
+
+   const registerWithMask = useHookFormMask(register);
 
    async function handleZipCodeBlur(event: React.FocusEvent<HTMLInputElement>) {
       setAddress({ city: '', state: '' });
@@ -17,7 +27,20 @@ function App() {
       if (response.ok) {
          const data = await response.json();
          setAddress({ city: data.city, state: data.state });
+         console.log(data);
       }
+   }
+
+   async function onSubmit(data: FieldValues) {
+      console.log('Form submitted', data);
+
+      /*       const response = await fetch('https://apis.codante.io/api/register-user/register', {
+         method: 'POST',
+         body: JSON.stringify(data),
+      });
+
+      const resData = await response.json();
+      console.log(resData); */
    }
 
    return (
@@ -28,8 +51,11 @@ function App() {
             </h1>
             <h2 className='text-zinc-300'>Formulário com React Hook Form e Zod</h2>
          </div>
-         <form className='w-full max-w-3xl bg-zinc-800 rounded-b-lg shadow-xl p-6 space-y-6'>
-            <fieldset className='flex flex-col gap-5 bg-zinc-700/20 border border-zinc-700 rounded-lg p-5'>
+         <form
+            onSubmit={handleSubmit(onSubmit)}
+            className='w-full max-w-3xl bg-zinc-800 rounded-b-lg shadow-xl p-6 space-y-6'
+         >
+            <fieldset className='flex flex-col gap-3 bg-zinc-700/20 border border-zinc-700 rounded-lg p-5'>
                <legend className='pt-1 pb-1.5 px-3 md:px-8 bg-zinc-800 border border-zinc-700 rounded-lg text-lg font-semibold text-zinc-300'>
                   Dados Pessoais
                </legend>
@@ -44,10 +70,23 @@ function App() {
                   <input
                      type='text'
                      id='firstName'
-                     name='firstName'
-                     required
+                     {...register('firstName', {
+                        required: 'O Nome deve ser preenchido.',
+                        minLength: {
+                           value: 2,
+                           message: 'O Nome deve ter no mínimo 2 caracteres.',
+                        },
+                        maxLength: {
+                           value: 40,
+                           message: 'O Nome deve ter no máximo 40 caracteres.',
+                        },
+                     })}
+                     maxLength={40}
                      className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600'
                   />
+                  <p className='text-orange-400 text-sm px-1'>
+                     <ErrorMessage errors={errors} name='firstName' />
+                  </p>
                </div>
 
                <div className='space-y-2'>
@@ -60,10 +99,23 @@ function App() {
                   <input
                      type='text'
                      id='lastName'
-                     name='lastName'
-                     required
+                     {...register('lastName', {
+                        required: 'O Sobrenome deve ser preenchido.',
+                        minLength: {
+                           value: 2,
+                           message: 'O Sobrenome deve ter no mínimo 2 caracteres.',
+                        },
+                        maxLength: {
+                           value: 40,
+                           message: 'O Sobrenome deve ter no máximo 40 caracteres.',
+                        },
+                     })}
+                     maxLength={40}
                      className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600'
                   />
+                  <p className='text-orange-400 text-sm px-1'>
+                     <ErrorMessage errors={errors} name='lastName' />
+                  </p>
                </div>
 
                <div className='space-y-2'>
@@ -76,11 +128,19 @@ function App() {
                   <input
                      type='tel'
                      id='phone'
-                     name='phone'
-                     required
-                     ref={withMask('(99) 99999-9999')}
+                     {...registerWithMask('phone', '(99) 99999-9999', {
+                        required: 'O Telefone celular deve ser preenchido.',
+                        pattern: {
+                           value: /^\(\d{2}\) \d{5}-\d{4}$/,
+                           message: 'Telefone celular inválido.',
+                        },
+                     })}
+                     maxLength={15}
                      className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600'
                   />
+                  <p className='text-orange-400 text-sm px-1'>
+                     <ErrorMessage errors={errors} name='phone' />
+                  </p>
                </div>
 
                <div className='space-y-2'>
@@ -93,11 +153,19 @@ function App() {
                   <input
                      type='text'
                      id='cpf'
-                     name='cpf'
-                     required
-                     ref={withMask('999.999.999-99')}
+                     {...registerWithMask('cpf', '999.999.999-99', {
+                        required: 'O CPF deve ser preenchido.',
+                        pattern: {
+                           value: /^\d{3}\.\d{3}\.\d{3}-\d{2}$/,
+                           message: 'CPF inválido.',
+                        },
+                     })}
+                     maxLength={14}
                      className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600'
                   />
+                  <p className='text-orange-400 text-sm px-1'>
+                     <ErrorMessage errors={errors} name='cpf' />
+                  </p>
                </div>
 
                <div className='space-y-2'>
@@ -110,12 +178,20 @@ function App() {
                   <input
                      type='text'
                      id='cep'
-                     name='cep'
-                     required
-                     ref={withMask('99999-999')}
-                     onBlur={handleZipCodeBlur}
+                     {...registerWithMask('zipcode', '99999-999', {
+                        required: 'O CEP deve ser preenchido.',
+                        pattern: {
+                           value: /^\d{5}-\d{3}$/,
+                           message: 'CEP inválido.',
+                        },
+                        onBlur: handleZipCodeBlur,
+                     })}
+                     maxLength={9}
                      className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600'
                   />
+                  <p className='text-orange-400 text-sm px-1'>
+                     <ErrorMessage errors={errors} name='zipcode' />
+                  </p>
                </div>
 
                <div className='grid grid-cols-1 md:grid-cols-[minmax(200px,_1fr)_150px] gap-4'>
@@ -129,10 +205,22 @@ function App() {
                      <input
                         type='text'
                         id='address'
-                        name='address'
-                        required
+                        {...register('address', {
+                           required: 'O Endereço deve ser preenchido.',
+                           minLength: {
+                              value: 2,
+                              message: 'O Endereço deve ter no mínimo 2 caracteres.',
+                           },
+                           maxLength: {
+                              value: 100,
+                              message: 'O Endereço deve ter no máximo 100 caracteres.',
+                           },
+                        })}
                         className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600'
                      />
+                     <p className='text-orange-400 text-sm px-1'>
+                        <ErrorMessage errors={errors} name='address' />
+                     </p>
                   </div>
 
                   <div className='space-y-2'>
@@ -145,9 +233,23 @@ function App() {
                      <input
                         type='text'
                         id='addressNumber'
-                        name='addressNumber'
+                        {...register('addressNumber', {
+                           maxLength: {
+                              value: 6,
+                              message:
+                                 'O número do endereço deve ter no máximo 6 caracteres.',
+                           },
+                           pattern: {
+                              value: /^\d{1,6}$/,
+                              message: 'Número inválido.',
+                           },
+                        })}
+                        maxLength={6}
                         className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600'
                      />
+                     <p className='text-orange-400 text-sm px-1'>
+                        <ErrorMessage errors={errors} name='addressNumber' />
+                     </p>
                   </div>
                </div>
 
@@ -162,9 +264,9 @@ function App() {
                      <input
                         type='text'
                         id='city'
-                        required
-                        disabled
+                        {...register('city')}
                         value={address.city}
+                        disabled
                         className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600 disabled:bg-zinc-800'
                      />
                   </div>
@@ -179,16 +281,16 @@ function App() {
                      <input
                         type='text'
                         id='state'
-                        required
-                        disabled
+                        {...register('state')}
                         value={address.state}
+                        disabled
                         className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600 disabled:bg-zinc-800'
                      />
                   </div>
                </div>
             </fieldset>
 
-            <fieldset className='flex flex-col gap-5 bg-zinc-700/20 border border-zinc-700 rounded-lg p-5'>
+            <fieldset className='flex flex-col gap-3 bg-zinc-700/20 border border-zinc-700 rounded-lg p-5'>
                <legend className='pt-1 pb-1.5 px-3 md:px-8 bg-zinc-800 border border-zinc-700 rounded-lg text-lg font-semibold text-zinc-300'>
                   Credenciais
                </legend>
@@ -203,10 +305,18 @@ function App() {
                   <input
                      type='email'
                      id='email'
-                     name='email'
-                     required
+                     {...register('email', {
+                        required: 'O E-mail deve ser preenchido.',
+                        pattern: {
+                           value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                           message: 'E-mail inválido.',
+                        },
+                     })}
                      className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600'
                   />
+                  <p className='text-orange-400 text-sm px-1'>
+                     <ErrorMessage errors={errors} name='email' />
+                  </p>
                </div>
 
                <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
@@ -217,14 +327,27 @@ function App() {
                      >
                         <span>*</span>Senha
                      </label>
-                     <div className='relative'>
+                     <div className='relative space-y-2'>
                         <input
                            type={isPasswordVisible ? 'text' : 'password'}
                            id='password'
-                           name='password'
-                           required
+                           {...register('password', {
+                              required: 'A Senha deve ser preenchida.',
+                              minLength: {
+                                 value: 8,
+                                 message: 'A Senha deve ter no mínimo 8 caracteres.',
+                              },
+                              maxLength: {
+                                 value: 24,
+                                 message: 'A Senha deve ter no máximo 24 caracteres.',
+                              },
+                           })}
+                           maxLength={24}
                            className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600'
                         />
+                        <p className='text-orange-400 text-sm px-1'>
+                           <ErrorMessage errors={errors} name='password' />
+                        </p>
                         <span className='absolute right-3 top-2'>
                            <button
                               type='button'
@@ -253,14 +376,29 @@ function App() {
                      >
                         <span>*</span>Confirmar senha
                      </label>
-                     <div className='relative'>
+                     <div className='relative space-y-2'>
                         <input
                            type={isConfirmPasswordVisible ? 'text' : 'password'}
                            id='confirmPassword'
-                           name='confirmPassword'
-                           required
+                           {...register('password_confirmation', {
+                              required: 'A Confirmação de senha deve ser preenchida.',
+                              minLength: {
+                                 value: 8,
+                                 message:
+                                    'A Confirmação deve ter no mínimo 8 caracteres.',
+                              },
+                              maxLength: {
+                                 value: 24,
+                                 message:
+                                    'A Confirmação deve ter no máximo 24 caracteres.',
+                              },
+                           })}
+                           maxLength={24}
                            className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600'
                         />
+                        <p className='text-orange-400 text-sm px-1'>
+                           <ErrorMessage errors={errors} name='password_confirmation' />
+                        </p>
                         <span className='absolute right-3 top-2'>
                            <button
                               type='button'
@@ -291,7 +429,7 @@ function App() {
                </div>
             </fieldset>
 
-            <fieldset className='flex flex-col gap-5 bg-zinc-700/20 border border-zinc-700 rounded-lg p-5'>
+            <fieldset className='flex flex-col gap-1 bg-zinc-700/20 border border-zinc-700 rounded-lg p-5'>
                <legend className='pt-1 pb-1.5 px-3 md:px-8 bg-zinc-800 border border-zinc-700 rounded-lg text-lg font-semibold text-zinc-300'>
                   Termos de Uso
                </legend>
@@ -304,8 +442,9 @@ function App() {
                      <input
                         type='checkbox'
                         id='terms'
-                        name='terms'
-                        required
+                        {...register('terms', {
+                           required: 'Os termos de uso devem ser aceitos.',
+                        })}
                         className='w-4 h-4 text-gray-600 bg-zinc-700 border-zinc-600 rounded focus:outline-none focus:ring-2 focus:ring-teal-600 accent-teal-400'
                      />
                      <span className='text-sm text-zinc-300'>
@@ -319,15 +458,26 @@ function App() {
                      </span>
                   </label>
                </div>
+               <p className='text-orange-400 text-sm px-1'>
+                  <ErrorMessage errors={errors} name='terms' />
+               </p>
             </fieldset>
 
             <p className='text-sm text-zinc-400'>* Campos obrigatórios</p>
 
             <button
                type='submit'
-               className='w-full bg-teal-800 text-white font-semibold py-2 px-4 rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2 focus:ring-offset-zinc-800 transition-colors cursor-pointer'
+               disabled={isSubmitting}
+               className='flex items-center justify-center gap-2 w-full bg-teal-800 text-white font-semibold py-2 px-4 rounded-md hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-600 focus:ring-offset-2 focus:ring-offset-zinc-800 transition-colors cursor-pointer disabled:opacity-50 disabled:cursor-not-allowed'
             >
-               Cadastrar
+               {isSubmitting ? (
+                  <>
+                     <SpinnerGap size={20} className='animate-spin' />
+                     Cadastrando
+                  </>
+               ) : (
+                  'Cadastrar'
+               )}
             </button>
          </form>
       </main>
