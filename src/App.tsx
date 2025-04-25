d34d35ab5d8c247854e3c39be01f4ec09f29357b
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2025-04-24 21:20:27 -0300

    Add the maxLength property to the input tag of the Address field

--- src/App.tsx
+++ src/App.tsx
@@ -280,6 +280,7 @@ function App() {
                               message: 'O Endereço deve ter no máximo 100 caracteres.',
                            },
                         })}
+                        maxLength={100}
                         className='w-full px-3 py-2 bg-zinc-900 border border-zinc-600 rounded-md text-teal-400 focus:outline-none focus:ring-2 focus:ring-teal-600'
                      />
                      <p className='text-orange-400 text-sm px-1'>
